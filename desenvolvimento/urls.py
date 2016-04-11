ffad0edbc09b628ce0ec501888f05b525fc7d618
humberto <humberto@localhost.localdomain>
2016-04-11 14:49:45 -0300

    Atualização das urls para casar curso e detalhes de curso

--- desenvolvimento/urls.py
+++ desenvolvimento/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
       url(r'^eventos', views.eventos, name='eventos'),
       url(r'^(?P<projeto_professor>[0-9]+)/$', views.details, name='details'),
       url(r'^(?P<projeto_projeto>.+)/$', views.detailsProjeto, name='detailsProjeto'),
-      #modificar 
+      #modificar
       # url(r'^(?P<projeto_projeto>[0-9]+)/projetos/$', views.detailsProjeto, name='detailsProjeto'),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
